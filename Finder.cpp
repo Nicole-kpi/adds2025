b03e375d322d4750872436dbd34944ea5d02c5a4
ZiYi Wang <ziyi.wang03@student.adelaide.edu.au>
2025-08-27 19:48:16 +0930

    comment

--- Finder.cpp
+++ Finder.cpp
@@ -3,24 +3,29 @@ using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
-    if (s2.empty()) return result;
 
-    size_t searchStart = 0;
+    vector<string> prefixes(s2.size());
+    for (size_t i = 1; i <= s2.size(); i++) {
+        prefixes[i - 1] = s2.substr(0, i);
+    }
 
     for(size_t i = 1; i <= s2.size(); i++) {
-        string prefix = s2.substr(0, i);
-        size_t found = s1.find(prefix, searchStart);
+        size_t found = s1.find(s2.substr(0, i));
+    
         if (found != string::npos) {
             result.push_back(found);
         } else {
             result.push_back(-1);
-            for (size_t j = i; j <= s2.size(); j++) {
-                result.push_back(-1);
-            }
-            break;
         }
     }
 
-return result;
-
+    if (!s2.empty()) {
+        size_t pos = s1.find(s2);
+        while (pos != string::npos) {
+            result.push_back(static_cast<int>(pos));
+            pos = s1.find(s2, pos + 1);
+        }
+    }
+    
+    return result;
 }
