10681674e8b1c26cf089523daf13fedfc58c6c50
ZiYi Wang <ziyi.wang03@student.adelaide.edu.au>
2025-10-08 10:01:18 +1030

    comment

---
+++ PrefixMatches.h
@@ -0,0 +1,26 @@
+#ifndef PREFIXMATCHES_H
+#define PREFIXMATCHES_H
+#include <string>
+
+class PrefixMatches{
+private:
+    struct Node{
+        Node* child[2];
+        int router;
+        Node() : child{nullptr, nullptr}, router(-1) {}
+        ~Node(){
+            delete child[0];
+            delete child[1];
+        }
+    };
+
+    Node* root;
+    static int bitIndex(char c);
+
+public:
+    PrefixMatches();
+    ~PrefixMatches();
+    void insert(const std::string& address, int routerNuber);
+    int selectRouter(const std::string& networkAddress) const;
+};
+#endif
